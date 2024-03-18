02897b6d728c7d62fbbf779b33b3e3e54acf1317
Denys Sttrilchuk <denia876@gmail.com>
2024-03-18 17:49:03 +0200

    working with searching

--- src/components/HeaderContainer/Header.tsx
+++ src/components/HeaderContainer/Header.tsx
@@ -1,15 +1,38 @@
-import {NavLink} from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
+import {useState} from "react";
 
 import  css from './Header.module.css';
+import {searchActions} from "../../store";
+import {useAppDispatch} from "../../hooks";
 
 
 const Header = () => {
+    const [query, setQuery] = useState<string>("");
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+
+    const handleSearch = () => {
+        dispatch(searchActions.getAll(query))
+        navigate('search')
+    };
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setQuery(e.target.value);
+    };
     return (
         <div className={css.Header}>
             <h1>Imovie</h1>
             <NavLink to={'movies'}>Movies</NavLink>
             <NavLink to={'genres'}>Genres</NavLink>
-            <NavLink to={'search'}>Search</NavLink>
+            <div className={css.input_style}>
+                <input
+                    type="text"
+                    placeholder="Search movies..."
+                    value={query}
+                    onChange={handleInputChange}
+                />
+                <button onClick={handleSearch}>Search</button>
+            </div>
         </div>
     );
 };
--- src/components/SearchMoviesContainer/SearchMovies/SearchMovies.tsx
+++ src/components/SearchMoviesContainer/SearchMovies/SearchMovies.tsx
@@ -1,41 +1,26 @@
+import {useEffect} from "react";
+
 import {useAppDispatch, useAppSelector} from "../../../hooks";
-import {useEffect, useState} from "react";
 import {searchActions} from "../../../store";
 import {Movie} from "../../MoviesListConttainer";
+import css from './SearchMovies.module.css';
 
 const SearchMovies = () => {
     const {movies} = useAppSelector(state => state.search);
     const dispatch = useAppDispatch();
-    const [query, setQuery] = useState<string>("");
+
 
     useEffect(() => {
         dispatch(searchActions.getAll(''))
     }, [dispatch]);
 
-    const handleSearch = () => {
-        dispatch(searchActions.getAll(query))
-    };
 
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setQuery(e.target.value);
-    };
 
     return (
-        <div>
-            <div>
-                <input
-                    type="text"
-                    placeholder="Search movies..."
-                    value={query}
-                    onChange={handleInputChange}
-                />
-                <button onClick={handleSearch}>Search</button>
-            </div>
-            <div>
+        <div className={css.Movies_list}>
                 {movies && movies.map((movie) => (
                     <Movie key={movie.id} movie={movie} />
                 ))}
-            </div>
         </div>
     );
 };
