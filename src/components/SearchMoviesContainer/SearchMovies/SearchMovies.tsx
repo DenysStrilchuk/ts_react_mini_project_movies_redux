687c89dd57999ee91e727cc7540800e758cc12ba
Denys Sttrilchuk <denia876@gmail.com>
2024-03-23 09:11:22 +0200

    doing logic with button

--- src/components/SearchMoviesContainer/SearchMovies/SearchMovies.tsx
+++ src/components/SearchMoviesContainer/SearchMovies/SearchMovies.tsx
@@ -1,18 +1,28 @@
 import {useAppSelector} from "../../../hooks";
 import {Movie} from "../../MoviesListConttainer";
 import css from './SearchMovies.module.css';
+import {useNavigate} from "react-router-dom";
 
 
 
 const SearchMovies = () => {
     const { movies } = useAppSelector(state => state.search);
+    const navigate = useNavigate();
 
+    const goBack = () => {
+        navigate('/movies');
+    };
 
     return (
-        <div className={css.Movies_list}>
+        <div>
+            <div className={css.backBtn}>
+                <button onClick={goBack}>Back</button>
+            </div>
+            <div className={css.Movies_list}>
                 {movies && movies.map((movie) => (
-                    <Movie key={movie.id} movie={movie} />
+                    <Movie key={movie.id} movie={movie}/>
                 ))}
+            </div>
         </div>
     );
 };
