b800efef5f072f938ea2eb3caa5b10ba4ee84f50
Denys Sttrilchuk <denia876@gmail.com>
2024-03-23 12:27:52 +0200

    doing logic with budge and  movies

--- src/components/GenresInfoContainer/Genre/Genre.tsx
+++ src/components/GenresInfoContainer/Genre/Genre.tsx
@@ -2,7 +2,7 @@ import {FC, PropsWithChildren} from 'react';
 import {IGenre} from "../../../interfaces";
 import {Badge, Button} from "react-bootstrap";
 import css from './Genre.module.css';
-import {useNavigate} from "react-router-dom";
+import {useNavigate, useLocation} from "react-router-dom";
 
 interface IProps extends PropsWithChildren {
     genre:IGenre
@@ -11,12 +11,17 @@ interface IProps extends PropsWithChildren {
     count: number;
 }
 
+
+
 const Genre: FC<IProps> = ({genre,  isActive, count}) => {
     const {id, name} = genre;
     const navigate = useNavigate();
+    const location = useLocation();
+
+    const isGenresPage = location.pathname.startsWith('/genres');
     return (
         <div>
-            <Button className={`${css.genre_badge} ${isActive ? css.active : ''}`}
+            <Button className={`${css.genre_badge} ${isActive && isGenresPage ? css.active : ''}`}
                     onClick={() => navigate(`/genres/${id}`)}
                     variant="primary">
                 {name}<Badge className={css.numbers} bg="secondary"> {count}</Badge>
